43ab8753f786bce71ce4b42fe5912ae2ec30e2fc
Mreza2020 <mreza_4040@outlook.com>
2024-09-30 12:28:31 -0700

    Calculator

--- Calculator.go
+++ Calculator.go
@@ -1,5 +1,53 @@
 package main
 
+import "fmt"
+
+var f int
+
 func main() {
-	print("hello world")
+	var d string
+	var x int
+	var y int
+
+	fmt.Println("What math operation do you want to perform?")
+	fmt.Println("+ , - , * , / , %")
+	fmt.Scan(&d) //Taking mathematical operations
+	fmt.Print("The first value\t")
+	fmt.Scan(&x) //The first value
+	fmt.Printf("\n%v\n\n", d)
+	fmt.Print("The second value ")
+	fmt.Scan(&y) //The second value
+	if d == "+" {
+		positive(x, y)
+	}
+	if d == "-" {
+		minus(x, y)
+	}
+	if d == "*" {
+		multiplication(x, y)
+	}
+	if d == "/" {
+		division(x, y)
+	}
+	if d == "%" {
+		remaining(x, y)
+	}
+
+	fmt.Printf("%v", f)
+}
+
+func positive(x, y int) {
+	f = x + y
+}
+func minus(x, y int) {
+	f = x - y
+}
+func multiplication(x, y int) {
+	f = x * y
+}
+func division(x, y int) {
+	f = x / y
+}
+func remaining(x, y int) {
+	f = x % y
 }
