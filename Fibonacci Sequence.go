db4826f1030eb0f9b22e44144506e5cebc7e56e6
Mreza2020 <mreza_4040@outlook.com>
2024-10-02 10:34:43 -0700

    Calculator

---
+++ Fibonacci Sequence.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+var a int // Fibonacci input
+
+func main() {
+	fmt.Println("How many Fibonacci numbers do you want?\t")
+	fmt.Scan(&a)
+	println(Fibonacci(a))
+
+}
+
+func Fibonacci(a int) int {
+	if a <= 1 {
+		return a
+	} //To avoid stack overflow
+	return Fibonacci(a-1) + Fibonacci(a-2) //Formula ==   fn = (fn-1) + (fn-2)
+
+}
