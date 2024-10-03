7366121845e92eca021f30adc2173260b8e70217
Mreza2020 <mreza_4040@outlook.com>
2024-10-03 12:59:09 -0700

    Tribonacci

---
+++ Tribonacci Sequence.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+// Tribonacci function to calculate the n-th Tribonacci number
+func Tribonacci(n int) int {
+	if n == 0 {
+		return 0
+	} else if n == 1 || n == 2 {
+		return 1
+	}
+
+	// Create an array to store Tribonacci numbers
+	trib := make([]int, n+1)
+	trib[0], trib[1], trib[2] = 0, 1, 1
+
+	for i := 3; i <= n; i++ {
+		trib[i] = trib[i-1] + trib[i-2] + trib[i-3]
+	}
+
+	return trib[n]
+}
+
+func main() {
+	n := 5 // Example: Get the 10th Tribonacci number
+	fmt.Printf("The %d-th Tribonacci number is: %d\n", n, Tribonacci(n))
+}
